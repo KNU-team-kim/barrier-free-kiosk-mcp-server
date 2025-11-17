9cdfbc79b5f777558a48847e3ff718e141bce73c
anjdydhody <a01066444534@gmail.com>
2025-11-17 16:51:47 +0900

    feat: tool 내에서 단계 돌아가기 기능 구현
    #8

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from typing import Optional
 from pydantic import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from steps.move_in_steps import MOVE_IN_STEPS
-from steps.resident_registration_steps import RESIDENT_REGISTRATION_STEP
+from steps.resident_registration_steps import RESIDENT_REGISTRATION_STEPS
 from prompts.move_in_prompt import MOVE_IN_PROMPT
 from prompts.resident_registration_prompt import RESIDENT_REGISTRATION_PROMPT
 from interfaces.move_in_output import MoveInOutput
@@ -39,6 +39,7 @@ class StepOutput(BaseModel):
     ai_message: Optional[str] = Field(default=None)
     data: Optional[str] = Field(default=None)
     cancel: Optional[bool] = Field(default=None)
+    retry: Optional[int] = Field(default=None)
 
 async def retrieve_agent_chain(PROMPT: str):
     llm = ChatOpenAI(
@@ -63,6 +64,85 @@ async def retrieve_agent_chain(PROMPT: str):
     agent_chain = prompt | llm | output_parser
     return agent_chain
 
+async def retry_step_by_index(index, STEPS, agent_chain, chat_history, output, ctx, session_id):
+    step_name, _, step_prompt = STEPS[index]
+
+    user_message = ""
+    step_output = "nothing"
+    retrieve_output = True
+
+    while step_output == "nothing":
+        try:
+            ai_output = await agent_chain.ainvoke(
+                input={
+                    "input": user_message,
+                    "step_prompt": step_prompt,
+                    "retry_steps": "",
+                    "chat_history": chat_history
+                }
+            )
+
+            if ai_output.cancel:
+                return Output(message="취소되었습니다. 어떤 서비스를 원하시나요?", step_name="home")
+
+            if ai_output.data:
+                if step_name == "phone_number" and not fetch_check_phone_number(ai_output.data, output.name):
+                    ai_output.ai_message = "정보를 찾을 수 없습니다. 전화번호를 다시 말씀해주세요."
+                    ai_output.data = None
+                elif step_name == "before_sigungu":
+                    retrieve_output = False
+                    step_output = ai_output.data
+                    ai_output.data = fetch_retrieve_address(output.name, output.phone_number)
+                elif step_name == "after_building_number_sub" and ai_output.data == "null":
+                    retrieve_output = False
+                    ai_output.data = None
+                    step_output = ai_output.data
+                elif step_name == "other_service":
+                    retrieve_output = False
+                    step_output = None
+                    if ai_output.data == "null":
+                        ai_output.data = []
+                    else:
+                        ai_output.data = [part.strip() for part in ai_output.data.split(",")]
+                elif step_name == "registration_number" and not fetch_check_registration_number(ai_output.data):
+                    ai_output.ai_message = "정보를 찾을 수 없습니다. 주민등록번호를 다시 입력해주세요."
+                    ai_output.data = None
+                else:
+                    retrieve_output = False
+                    step_output = ai_output.data
+
+            response = await ctx.elicit(
+                message=json.dumps(
+                    {
+                        "session_id": session_id,
+                        "retrieve_output": retrieve_output,
+                        "data": ai_output.data,
+                        "ai_message": ai_output.ai_message,
+                        "step_name": step_name,
+                    }
+                ),
+                schema=ElicitationResponse,
+            )
+
+            match response:
+                case AcceptedElicitation(data=data):
+                    user_message = data.user_message
+                case DeclinedElicitation():
+                    break
+                case CancelledElicitation():
+                    break
+
+        except (anyio.ClosedResourceError, ConnectionError) as e:
+            logger.exception(e)
+            raise e
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
+    if hasattr(output, step_name):
+        setattr(output, step_name, step_output)
+
+
 @mcp_server.tool(name="move-in-conversation", title="전입신고")
 async def move_in_conversation(ctx: Context, session_id: Optional[str] = None) -> Output:
     """사용자가 '전입신고' 관련 요청을 할 때 사용됩니다. 전입신고 절차를 진행합니다."""
@@ -70,10 +150,14 @@ async def move_in_conversation(ctx: Context, session_id: Optional[str] = None) -
     move_in_output = MoveInOutput()
     agent_chain = await retrieve_agent_chain(MOVE_IN_PROMPT)
 
-    for step_name, step_prompt in MOVE_IN_STEPS:
+    for idx, (step_name, _, step_prompt) in enumerate(MOVE_IN_STEPS):
         user_message = ""
         step_output = "nothing"
         retrieve_output = True
+
+        retry_steps = ""
+        for i in range(idx):
+            retry_steps += f"{i}: {MOVE_IN_STEPS[i][1]}\n"
 
         while step_output == "nothing":
             try:
@@ -81,9 +165,22 @@ async def move_in_conversation(ctx: Context, session_id: Optional[str] = None) -
                     input={
                         "input": user_message,
                         "step_prompt": step_prompt,
-                        "chat_history": chat_history,
+                        "retry_steps": retry_steps,
+                        "chat_history": chat_history
                     }
                 )
+
+                if ai_output.retry != None:
+                    await retry_step_by_index(
+                        index=ai_output.retry, 
+                        STEPS=MOVE_IN_STEPS,
+                        agent_chain=agent_chain, 
+                        chat_history=chat_history, 
+                        output=move_in_output, 
+                        ctx=ctx, 
+                        session_id=session_id
+                    )
+                    continue
 
                 if ai_output.cancel:
                     return Output(message="취소되었습니다. 어떤 서비스를 원하시나요?", step_name="home")
@@ -154,10 +251,14 @@ async def resident_registration_conversation(ctx: Context, session_id: Optional[
     resident_registration_output = ResidentRegistrationOutput()
     agent_chain = await retrieve_agent_chain(RESIDENT_REGISTRATION_PROMPT)
 
-    for step_name, step_prompt in RESIDENT_REGISTRATION_STEP:
+    for idx, (step_name, _, step_prompt) in enumerate(RESIDENT_REGISTRATION_STEPS):
         user_message = ""
         step_output = "nothing"
         retrieve_output = True
+
+        retry_steps = ""
+        for i in range(idx):
+            retry_steps += f"{i}: {RESIDENT_REGISTRATION_STEPS[i][1]}\n"
 
         while step_output == "nothing":
             try:
@@ -173,9 +274,23 @@ async def resident_registration_conversation(ctx: Context, session_id: Optional[
                     input={
                         "input": user_message,
                         "step_prompt": new_step_prompt,
-                        "chat_history": chat_history,
+                        "retry_steps": retry_steps,
+                        "chat_history": chat_history
                     }
                 )
+
+                if ai_output.retry != None:
+                    await retry_step_by_index(
+                        index=ai_output.retry, 
+                        STEPS=RESIDENT_REGISTRATION_STEPS,
+                        agent_chain=agent_chain, 
+                        chat_history=chat_history, 
+                        output=resident_registration_output, 
+                        ctx=ctx, 
+                        session_id=session_id
+                    )
+                    user_message = ""
+                    continue
 
                 if ai_output.cancel:
                     return Output(message="취소되었습니다. 어떤 서비스를 원하시나요?", step_name="home")
